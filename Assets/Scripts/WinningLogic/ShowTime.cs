64dafabf5021063ed91f1ae317541a687d7b4302
Matvey <sinema3d00@gmail.com>
2024-10-04 15:12:03 +0300

    making UI

---
+++ Assets/Scripts/WinningLogic/ShowTime.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowTime : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private TimeController _timeController;
+
+    private void Update()
+    {
+        _timerText.text = "Time: " + _timeController.Timer.ToString("F2");
+    }
+} 
