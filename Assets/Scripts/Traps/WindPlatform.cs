926e541ad67623e98066172f6cbbae304facec3c
Matvey <sinema3d00@gmail.com>
2024-10-04 09:46:34 +0300

    made wind trap logic

---
+++ Assets/Scripts/Traps/WindPlatform.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class WindZone : MonoBehaviour
+{
+    [SerializeField] private LayerMask _playerLayer;
+    [SerializeField] private float _windForce = 10f;
+    [SerializeField] private float _changeDirectionInterval = 2f;
+    private float _timeSinceLastChange = 0f;
+    private Vector3 _windDirection;
+
+    void Start()
+    {
+        ChangeWindDirection();
+    }
+
+    void Update()
+    {
+        _timeSinceLastChange += Time.deltaTime;
+        if (_timeSinceLastChange >= _changeDirectionInterval)
+        {
+            ChangeWindDirection();
+            _timeSinceLastChange = 0f;
+        }
+    }
+
+    void ChangeWindDirection()
+    {
+        _windDirection = Random.Range(0, 2) == 0 ? Vector3.left : Vector3.right;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & _playerLayer) != 0)
+        {
+            if (other.TryGetComponent<CharacterController>(out var controller))
+            {
+                controller.Move(_windForce * Time.deltaTime * _windDirection);
+            }
+        }
+    }
+}
