d486f47e2807224e404cd9508c557e88af1a55aa
Matvey <sinema3d00@gmail.com>
2024-10-04 10:57:46 +0300

    add player health logic

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,8 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int Health {  get; private set; }
+    [SerializeField] private int _maxHealth = 3; 
+    public int Health { get; private set; }
+
+    public event Action OnPlayerDeath;  
+    public event Action<int> OnHealthChanged;  
+
+    private void Start()
+    {
+        Health = _maxHealth;  
+    }
+    private void OnEnable()
+    {
+        DamageTrap.OnDamageDealt += TakeDamage; 
+    }
+
+    private void OnDisable()
+    {
+        DamageTrap.OnDamageDealt -= TakeDamage; 
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (Health <= 0) return;  
+
+        Health = Mathf.Clamp(Health - damageAmount, 0, _maxHealth);  
+        OnHealthChanged?.Invoke(Health);
+
+        Debug.Log(Health);
+
+        if (Health == 0)
+        {
+            Die();  
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player has died.");  
+        OnPlayerDeath?.Invoke(); 
+    }
 }
